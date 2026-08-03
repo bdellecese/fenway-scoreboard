9e89337960308457c12e08e7703a7243cdf1b3b7
bdellecese <barry.dellecese@gmail.com>
2026-08-03 15:06:38 -0400

    Update loadRedSoxGame function for better logging

--- js/featured-game.js
+++ js/featured-game.js
@@ -1,7 +1,68 @@
 async function loadRedSoxGame() {
 
-    console.log("Featured game loading...");
+    console.log("Looking for Red Sox game...");
+
+
+    const yesterday = new Date();
+    yesterday.setDate(yesterday.getDate() - 1);
+
+
+    const apiDate = yesterday.toISOString().split("T")[0];
+
+
+    try {
+
+        const response = await fetch(
+            `https://statsapi.mlb.com/api/v1/schedule?sportId=1&date=${apiDate}`
+        );
+
+
+        const data = await response.json();
+
+
+        if (!data.dates.length) {
+
+            console.log("No games found.");
+            return;
+
+        }
+
+
+        const games = data.dates[0].games;
+
+
+        const redSoxGame = games.find(game =>
+            game.teams.home.team.id === 111 ||
+            game.teams.away.team.id === 111
+        );
+
+
+        if (!redSoxGame) {
+
+            console.log("No Red Sox game yesterday.");
+            return;
+
+        }
+
+
+        console.log("Red Sox game found:");
+        console.log(redSoxGame);
+
+
+        console.log("Game PK:");
+        console.log(redSoxGame.gamePk);
+
+
+    } catch (err) {
+
+        console.error(
+            "Unable to load Red Sox game:",
+            err
+        );
+
+    }
 
 }
+
 
 loadRedSoxGame();
